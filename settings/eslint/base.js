0edf7bfa7e5eb8c3c02932f3d95dbbeb5ce701c5
Gideon Kreitzer <gidhon@users.noreply.github.com>
2020-11-11 06:53:03 -0500

    improve config legibility

--- settings/eslint/base.js
+++ settings/eslint/base.js
@@ -5,12 +5,15 @@ module.exports = {
         'node': true,
         'browser': true,
     },
+
     extends: 'eslint:recommended',
     parser: '@babel/eslint-parser',
+
     parserOptions: {
         ecmaVersion: 2021,
         sourceType: 'module',
     },
+
     rules: {},
 
 };
