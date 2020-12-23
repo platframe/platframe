d0365f38de6f93c3191fb70117f79a09ea756aef
Gideon Kreitzer <gidhon@users.noreply.github.com>
2020-12-23 11:17:21 -0500

    update base ESLint config

--- settings/eslint/base.js
+++ settings/eslint/base.js
@@ -1,9 +1,9 @@
 module.exports = {
 
     env: {
-        'es6': true,
-        'node': true,
-        'browser': true,
+        es6: true,
+        node: true,
+        browser: true,
     },
 
     extends: 'eslint:recommended',
